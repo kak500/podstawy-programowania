59936ae114e53190b5d433d5f60dda2966a215e1
kak500 <36472752+kak500@users.noreply.github.com>
2019-10-03 11:42:33 +0200

    zadanie 1

---
+++ zadanie_1/zadanie_1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace zadanie_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b;
+            Console.WriteLine("To jest program który oblicza pole prostokąta");
+            Console.Write("Podaj bok a: ");
+            a = double.Parse(Console.ReadLine());
+
+            Console.Write("Podaj bok b: ");
+            b = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Pole prostokąta wynosi: {0} cm/kw", a * b);
+            Console.Read();
+        }
+    }
+}
