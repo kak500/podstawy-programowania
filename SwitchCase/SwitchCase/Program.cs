054e70633bfbddb97d5d48a2c3ef14beae258578
kak500 <36472752+kak500@users.noreply.github.com>
2019-10-10 11:17:15 +0200

    Switch Case

---
+++ SwitchCase/SwitchCase/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1. Pole kwadratu\n2. Pole koła");
+            Console.Write("Wybierz: ");
+
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    //Kwadrat
+                    Console.Clear();
+                    Console.WriteLine("Obliczanie pola kwadratu");
+                    Console.Write("Podaj długość: ");
+                    string LengthS = Console.ReadLine();
+                    if (double.TryParse(LengthS, out double Length))
+                    {
+                        Console.WriteLine("Pole kwadratu wynosi: {0}", Length*Length);
+                    }
+                    break;
+                case "2":
+                    //koło
+                    Console.Clear();
+                    Console.WriteLine("Obliczanie pola koła");
+                    Console.Write("Podaj promień: ");
+                    string RadiusS = Console.ReadLine();
+                    if (double.TryParse(RadiusS, out double Radius))
+                    {
+                        double Field = Math.PI * Math.Pow(Radius, 2.0);
+                        Console.WriteLine("Pole koła wynosi: {0:##.##}", Field);
+                    }
+                    break;
+                default:
+                    //default
+                    Console.WriteLine("Podałeś błędne dane");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
