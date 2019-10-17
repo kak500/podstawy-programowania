446946af8bca22275fc95cf168132beb38824c1d
kak500 <36472752+kak500@users.noreply.github.com>
2019-10-17 12:12:04 +0200

    Add files via upload

---
+++ Tablice/Tablice/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Tablice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n=10,i;
+            int[] array = new int[n];
+
+            for(i=0; i < array.Length; i++)
+            {
+                array[i] = i+1;
+            }
+
+            for (i = 0; i< array.Length; i++)
+            {
+                Console.Write("{0}, ",array[i]);
+            }
+
+            foreach (int el in array)
+            {
+                Console.WriteLine(el);
+            }
+            Console.Clear();
+
+
+            string[] colors = new string[5];
+            Console.WriteLine("Wprowadź kolory: ");
+            for(i=0; i< colors.Length; i++)
+            {
+                colors[i] = Console.ReadLine();
+            }
+            i = 1;
+            Console.Clear();
+            foreach(string color in colors)
+            {
+                Console.WriteLine("kolor{0}: {1}", i, color);
+                i++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
