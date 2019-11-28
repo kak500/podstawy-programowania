a8eda9c429ddfb4b5d310af0a3ca1b61616b817d
kak500 <36472752+kak500@users.noreply.github.com>
2019-11-28 12:13:57 +0100

    Add files via upload

---
+++ zadanie biblioteki/ColorLib/ColorLib/Class1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace ColorLib
+{
+    public class ColorClass
+    {
+        //public string ColorName;
+
+        public string[] ColorNames = {"red","green","blue","yellow"};
+
+        public void getColor()
+        {
+            Console.WriteLine("Podaj kolor: ");
+            string ColorName = Console.ReadLine();
+        }
+
+        public void PrintColors()
+        {
+            foreach (var item in ColorNames)
+            {
+                Console.WriteLine("{0}", item);
+            }
+        }
+
+        public void ChangeBackground(string ColorName)
+        {
+            if (ColorName == "red")
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+            }
+            if (ColorName == "green")
+            {
+                Console.BackgroundColor = ConsoleColor.Green;
+            }
+            if (ColorName == "blue")
+            {
+                Console.BackgroundColor = ConsoleColor.Blue;
+            }
+            if (ColorName == "yellow")
+            {
+                Console.BackgroundColor = ConsoleColor.Yellow;
+            }
+        }
+
+        public void ChangeForeground(string ColorName)
+        {
+            if (ColorName == "red")
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+            if (ColorName == "green")
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+            }
+            if (ColorName == "blue")
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+            }
+            if (ColorName == "yellow")
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+            }
+        }
+    }
+}
---
+++ zadanie biblioteki/Main/Main/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ColorLib;
+namespace Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ColorClass ColorManage = new ColorClass();
+
+            Console.WriteLine("Chcesz zmienić kolor tła czy tekstu?");
+            string Choice = Console.ReadLine();
+            
+            if (Choice == "tla" || Choice == "tła")
+            {
+                ColorManage.PrintColors();
+                ColorManage.getColor();
+                ColorManage.ChangeBackground("red");
+            }
+            if (Choice == "tekstu")
+            {
+                ColorManage.ChangeForeground("red");
+            }
+
+
+
+            Console.WriteLine("git");
+
+            Console.ReadKey();
+        }
+    }
+}
